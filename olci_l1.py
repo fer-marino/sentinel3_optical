5cb3b7aeb54c7da4c7f9b141d4dfe7bd11628389
Fernando <fernando.marino85@gmail.com>
2017-04-16 21:07:32 +0200

    refining

--- olci_l1.py
+++ olci_l1.py
@@ -1,26 +1,30 @@
 from numpy import float32, zeros
-import scipy.misc as im
-import math
+from scipy.misc import imsave
 from skimage import exposure
-import time
-import xarray as xr
+from time import time
+from xarray import open_dataset
+import math
+
 
 def process(product):
     # open radiance datasets
-    red = xr.open_dataset(product + "/Oa10_radiance.nc")["Oa10_radiance"]
-    green = xr.open_dataset(product + "/Oa05_radiance.nc")["Oa05_radiance"]
-    blue = xr.open_dataset(product + "/Oa03_radiance.nc")["Oa03_radiance"]
-
-    width = red.sizes["columns"]
-    height = red.sizes["rows"]
+    print("Loading data...")
+    start_time = time()
+    red = open_dataset(product + "/Oa10_radiance.nc")["Oa10_radiance"].values[:]
+    green = open_dataset(product + "/Oa05_radiance.nc")["Oa05_radiance"].values[:]
+    blue = open_dataset(product + "/Oa03_radiance.nc")["Oa03_radiance"].values[:]
 
     # ancillary data needed for TOA reflectance conversion
-    instrument_data = xr.open_dataset(product + "/instrument_data.nc")
-    tie_geometries = xr.open_dataset(product + "/tie_geometries.nc")
+    instrument_data = open_dataset(product + "/instrument_data.nc")
+    tie_geometries = open_dataset(product + "/tie_geometries.nc")
 
-    detector_index = instrument_data["detector_index"]
-    sza = tie_geometries["SZA"]
-    solar_flux = instrument_data["solar_flux"]
+    detector_index = instrument_data["detector_index"].values[:]
+    sza = tie_geometries["SZA"].values[:]
+    solar_flux = instrument_data["solar_flux"].values[:]
+
+    width = instrument_data["detector_index"].sizes["columns"]
+    height = instrument_data["detector_index"].sizes["rows"]
+    print("Loading completed in {w:.2f} seconds".format(w=time() - start_time))
 
     # initialize image
     img = zeros((height, width, 3), dtype=float32)
@@ -28,44 +32,47 @@ def process(product):
 
     # Set the RGB values
     print("Processing started. Dimension w:{w:d} h:{h:d}".format(w=width, h=height))
-    start_time = time.time()
+    start_time = time()
+    perf_time = time()
     counter = 0
     for y in range(img.shape[0]):
         for x in range(img.shape[1]):
             counter += 1
-            if not math.isnan(detector_index.values[y][x]):
-                detector = int(detector_index.values[y][x])
-                angle = sza.values[y][int(x/64)]
-                sol_flux_red = solar_flux.values[9][detector]
-                sol_flux_green = solar_flux.values[4][detector]
-                sol_flux_blue = solar_flux.values[2][detector]
+            if not math.isnan(detector_index[y][x]):
+                detector = int(detector_index[y][x])
+                angle = sza[y][int(x/64)]
+                sol_flux_red = solar_flux[9][detector]      # band 10 for red, has index 9
+                sol_flux_green = solar_flux[4][detector]    # band 5 for green, has index 4
+                sol_flux_blue = solar_flux[2][detector]     # band 3 for blue, has index 2
 
-                img[y][x][0] = math.pi * (red.values[y][x] / sol_flux_red) / math.cos(math.radians(angle))
-                img[y][x][1] = math.pi * (green.values[y][x] / sol_flux_green) / math.cos(math.radians(angle))
-                img[y][x][2] = math.pi * (blue.values[y][x] / sol_flux_blue) / math.cos(math.radians(angle))
+                img[y][x][0] = math.pi * (red[y][x] / sol_flux_red) / math.cos(math.radians(angle))
+                img[y][x][1] = math.pi * (green[y][x] / sol_flux_green) / math.cos(math.radians(angle))
+                img[y][x][2] = math.pi * (blue[y][x] / sol_flux_blue) / math.cos(math.radians(angle))
 
-                img_raw[y][x][0] = red.values[y][x]
-                img_raw[y][x][1] = green.values[y][x]
-                img_raw[y][x][2] = blue.values[y][x]
+                img_raw[y][x][0] = red[y][x]
+                img_raw[y][x][1] = green[y][x]
+                img_raw[y][x][2] = blue[y][x]
 
-        if time.time() - start_time > 30:
-            print("Processing row {row:d} {perc:.3f}% - Pixel per seconds: {perf:d}".format(row=y, perc=(y/height)*100, perf=int(counter / (time.time() - start_time))))
+        if time() - perf_time > 30:
+            print(" * * Processing row {row:d} {perc:.3f}% - Pixel per seconds: {perf:d}".format(row=y, perc=(y/height)*100, perf=int(counter / (
+            time() - perf_time))))
             counter = 0
-            start_time = time.time()
+            perf_time = time()
 
-    print("Processing finished")
-    # Display the image
-    # im.imshow(img)
-    im.imsave(product+".png", img)
-    im.imsave(product+"_raw.png", img)
+    print("Processing finished in {perf:.2f} seconds ".format(perf=(time() - start_time)))
+
+    print("Saving raw...")
+    imsave(product + "/toa_reflectance.png", img)
     # adjust contrast
+    print("Equalizing...")
+    start_time = time()
     img_equalized = exposure.equalize_hist(img, nbins=1024)
     img_equalized_raw = exposure.equalize_hist(img_raw, nbins=1024)
-    # im.imshow(img_equalized)
 
     # Save the image
-    im.imsave(product+"_equalized.png", img_equalized)
-    im.imsave(product+"_equalized_raw.png", img_equalized_raw)
+    imsave(product + "/toa_equalized.png", img_equalized)
+    imsave(product + "/radiance_equalized.png", img_equalized_raw)
+    print("done in {perf:.2f} seconds ".format(perf=(time() - start_time)))
 
 
 if __name__ == "__main__":
